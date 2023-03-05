e4c318283836aa32d5e7b8afea8c92e816ca2702
whoisdon <102386944+whoisdon@users.noreply.github.com>
2023-03-04 23:00:49 -0300

    Create HomeRoute.js

---
+++ src/Routes/Get/HomeRoute.js
@@ -0,0 +1,15 @@
+import MapRoutes from '../../Handlers/Routes.js';
+
+export default class extends MapRoutes {
+  constructor(app) {
+    super(app, {
+        name: '/',
+        method: 'get',
+        static: 'src/Client/Render'
+    });
+  }
+
+  execute(req, res) {
+    res.sendFile(`${process.cwd()}/src/Client/Render/index.html`);
+  }
+}
