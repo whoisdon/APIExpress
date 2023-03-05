349883a94d3a7ac2d69696ba832f00adf55dbc84
whoisdon <102386944+whoisdon@users.noreply.github.com>
2023-03-05 17:47:55 -0300

    Update Register.js

--- src/Handlers/Register.js
+++ src/Handlers/Register.js
@@ -4,15 +4,15 @@ import express from 'express';
 import Register from './Routes.js';
 
 export default class extends Register {
-    constructor(app) {
-        super(app);
+  constructor(app) {
+    super(app);
   }
 
   async registerRoutes(path = 'src/Routes') {
     const categories = readdirSync(path);
     for (const category of categories) {
       const commands = readdirSync(`${path}/${category}`);
-    
+
       for (const command of commands) {
         const commandFile = join(process.cwd(), `${path}/${category}/${command}`);
         const { default: MapRoutes } = await import(commandFile);
@@ -21,9 +21,23 @@ export default class extends Register {
         cmd.options.json ? this.app.use(express.json()) : false;
         cmd.options.static ? this.app.use(express.static(`${process.cwd()}/${cmd.options.static}`)) : false;
 
-        const callback = (req, res, next) => cmd.execute(req, res, next);
+        const callback = (req, res, next) => cmd.execute(req, res, next);   
         this.app[cmd.options.method](cmd.options.name, callback)
+
+        this.erro404()
       }
     }
+  }  
+
+  erro404() {
+    this.app.use(express.static(`${process.cwd()}/src/Client/Error`))
+    
+    this.app.use(function(req, res, next) {
+      res.status(404).sendFile(`${process.cwd()}/src/Client/Error/index.html`, function(err) {
+        if (err) {
+          next(err);
+        }
+      });
+    })
   }
 }
