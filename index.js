00e265a2ea2c1033bc552d6022d0a78e6e02c967
whoisdon <102386944+whoisdon@users.noreply.github.com>
2023-03-04 23:01:30 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-var express = require("express");
-var app = express();
-
-app.use(express.json());
-
-require("./src/handler")(app);
-
-var { getListenConsole } = require('./src/functions/getListenConsole')
-var port = process.env.PORT || 3000;
-
-app.listen(port, () => { getListenConsole(port) });
-
-module.exports = app;
+import './src/main.js';
